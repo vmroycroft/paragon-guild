e3754620a2e4ea02001217d7e4ef395f602bddd4
Vanessa Roycroft <vmroycroft@gmail.com>
2019-11-24 18:21:44 -0500

    Work in progress home page - Added emblem and colors

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,26 @@
 <template>
-	<div id="app">
+	<div id="app" v-scroll="onScroll">
 		<v-app>
 			<v-navigation-drawer app>
 				<!-- -->
 			</v-navigation-drawer>
 
-			<v-app-bar
-				app
-				flat
-				color="rgba(0,0,0,0.85)"
-				class="d-none d-md-flex justify-center"
-			>
+			<v-app-bar app flat color="#0d181f" class="d-none d-md-flex justify-center">
+				<v-toolbar-title class="paragon-font hidden animated fadeOut">Paragon</v-toolbar-title>
 				<v-btn text>Home</v-btn>
 				<v-btn text>Forum</v-btn>
 				<v-btn text>Classes</v-btn>
-				<v-img
-					src="./assets/images/tabard.png"
-					max-height="55"
-					max-width="55"
-					:contain="true"
-				></v-img>
+				<v-img src="./assets/images/tabard.png" max-height="55" max-width="55" :contain="true"></v-img>
 				<v-btn text>Apply</v-btn>
 				<v-btn text>Events</v-btn>
 				<v-btn text>Inventory</v-btn>
 			</v-app-bar>
 
 			<v-content>
-				<v-container fluid>
-					<router-view></router-view>
-				</v-container>
+				<!-- <v-container> -->
+				<!-- <v-container fluid> -->
+				<router-view></router-view>
+				<!-- </v-container> -->
 			</v-content>
 
 			<!-- <v-footer app> </v-footer> -->
@@ -37,16 +29,24 @@
 </template>
 
 <script>
+import EventBus from './EventBus';
+
 export default {
-	name: "app"
+	name: 'app',
+	methods: {
+		onScroll() {
+			EventBus.$emit('scroll');
+		}
+	}
 };
 </script>
 
 <style>
+@import '~animate.css';
+
 @font-face {
-	font-family: "New Vera";
-	src: url("~/assets/fonts/New Vera/New Vera.woff2") format("woff2"),
-		url("~/assets/fonts/New Vera/New Vera.woff") format("woff");
+	font-family: 'New Vera';
+	src: url('~/assets/fonts/New Vera/New Vera.woff2') format('woff2'), url('~/assets/fonts/New Vera/New Vera.woff') format('woff');
 	font-weight: 300;
 	font-style: normal;
 }
@@ -54,9 +54,58 @@ export default {
 html,
 body,
 .v-application {
-	font-family: "Titillium Web", "Roboto", sans-serif !important;
+	font-family: 'Titillium Web', 'Roboto', sans-serif !important;
 	font-size: 16px; /* fallback if calc() is not supported */
-	font-size: responsive 14px 16px; /* https://www.rucksackcss.org/docs/#responsive-type */
+	font-size: responsive 16px 18px; /* https://www.rucksackcss.org/docs/#responsive-type */
 	line-height: 1.5;
+}
+
+hr {
+	background: url(~/assets/images/emblem.png) no-repeat top center;
+	background-size: contain;
+	display: block;
+	height: 18px;
+	border: 0;
+	position: relative;
+	width: 50%;
+	margin: 0 auto;
+}
+
+hr:before,
+hr:after {
+	content: '';
+	display: block;
+	position: absolute;
+	background: #101e3c;
+	height: 2px;
+	top: 8px;
+}
+
+hr:before {
+	left: 0;
+	right: 50%;
+	margin-right: 25px;
+	background: linear-gradient(to right, rgb(13, 24, 31, 0), rgba(13, 24, 31, 0.5), rgba(13, 24, 31, 1));
+}
+
+hr:after {
+	right: 0;
+	left: 50%;
+	margin-left: 25px;
+	background: linear-gradient(to left, rgb(13, 24, 31, 0), rgba(13, 24, 31, 0.5), rgba(13, 24, 31, 1));
+}
+
+ul {
+	margin-bottom: 16px;
+}
+
+.hidden {
+	visibility: hidden;
+}
+
+.paragon-font {
+	font-family: 'New Vera';
+	font-weight: 100;
+	color: #ad8965;
 }
 </style>
